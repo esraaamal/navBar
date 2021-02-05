ca2bdd00ee29bf8b18bd51f66059ee6a78c012dd
esraaamal <ma.esraa@hotmail.com>
2021-02-05 15:53:02 +0200

    f

--- js/main.js
+++ js/main.js
@@ -23,7 +23,15 @@ $('#slider-carousel').carouFredSel({
         pageAnchorBuilder:false
     }
 });
+$(window).scroll(function(){
+var top=$(window).scrollTop();
+if(top>=60){
+    $("header").addClass('secondary');
+}else if($("header").hasClass('secondary')){
+    $("header").removeClass('secondary');
+}
 
+});
 
 
 });
